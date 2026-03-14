e8a3fd676f0b95d6292d00d581ac923fc8be0d69
Philip <m.p.o.andersson@gmail.com>
2026-03-14 13:42:50 +0100

    Add hero image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,12 @@
 </script>
 
 <section id="hero">
-	<h1 class="name">
-		<span class="first-name">Philip</span> <span class="last-name">Andersson</span>
-	</h1>
-	<h2 class="title">Junior Frontend Developer</h2>
+	<div class="background-image">
+		<h1 class="name">
+			<span class="first-name">Philip</span> <span class="last-name">Andersson</span>
+		</h1>
+		<h2 class="title">Junior Frontend Developer</h2>
+	</div>
 </section>
 
 <style>
@@ -14,6 +16,13 @@
 		flex-direction: column;
 		margin: 0 auto;
 		padding: 0 5em 0 5em;
+	}
+	.background-image {
+		background-image: url('/hero.jpg');
+		background-size: cover;
+		background-position: center;
+		width: 80vw;
+		height: 80vh;
 	}
 	.name {
 		font-size: 8vw;
@@ -27,5 +36,7 @@
 	.title {
 		font-size: 2rem;
 		text-align: center;
+		color: white;
+		align-items: flex-end;
 	}
 </style>
